84a8545f1b70a769421878adf4fe5b8399c672ca
Rafael Ospina <rafaelospiname@gmail.com>
2018-04-14 17:23:58 -0500

    Sin terminar

---
+++ RafaelOspina-AirCombat/Assets/Scripts/Airplane.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0, 0, 5f * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0, 0.5f, 0);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0, -0.5f, 0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (-0.5f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (0.5f, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject missile = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+			missile.transform.SetParent (this.gameObject.transform);
+			missile.transform.localPosition = new Vector3 (0.347f, -0.032f, -0.233f);
+			missile.transform.SetParent (null);
+		}
+	}
+}
---
+++ RafaelOspina-AirCombat/Assets/Scripts/Missile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0, 0, 50.0f * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.collider.gameObject.tag == "Enemy") {
+
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/Explosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+
+		if (collision.collider.gameObject.tag == "Ally") {
+
+			GameObject son = collision.collider.gameObject.transform.Find ("LuzVerde").gameObject;
+
+			son.GetComponent <Light> ().color = new Color (0, 0, 1);
+			Destroy (this.gameObject);
+
+		}
+	}
+}
